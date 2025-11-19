6209212a48710065439cf1834786217cd9d7ec88
DavidSeba <mugisaseba@gmail.com>
2025-11-20 00:22:23 +0300

    COMMIT #1 ON BRANCH #4
    Started from here, the process of adding new information of existed user, is the Phone Number, as you can seen this is just the first process.

--- USER Manager/src/UserProfileManager/UserInfo.java
+++ USER Manager/src/UserProfileManager/UserInfo.java
@@ -4,11 +4,13 @@ public class UserInfo {
     String UserName;
     String UserRegNumber;
     String UserFacultyName;
+    String UserPhoneNumber;
 
-    public UserInfo(String userName, String userRegNumber, String userFacultyName) {
+    public UserInfo(String userName, String userRegNumber, String userFacultyName,  String UserPhoneNumber) {
         this.UserName = userName;
         this.UserRegNumber = userRegNumber;
         this.UserFacultyName = userFacultyName;
+        this.UserPhoneNumber = UserPhoneNumber;
     }
 
     public String getUserName() {
@@ -21,5 +23,8 @@ public class UserInfo {
 
     public String getUserFacultyName() {
         return UserFacultyName;
+    }
+    public String getPhoneNumber() {
+        return UserPhoneNumber;
     }
 }
